927ba62a2631982abdc57ec90d6c5464e3d55087
Igor Białek <igorbialek0@gmail.com>
2023-09-30 18:59:28 +0200

    working connection

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,30 @@
 import Header from "../components/Header";
 import "../styles/globals.css";
 import { useEffect, useState } from "react";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export default function App({ Component, pageProps }) {
-    const [activePage, setActivePage] = useState("userForm");
+  const [activePage, setActivePage] = useState("userForm");
 
-    const navigateToPage = (page) => {
-        if (localStorage.getItem("weight") == undefined) return;
-        setActivePage(page);
-    };
+  const navigateToPage = (page) => {
+    if (localStorage.getItem("weight") == undefined) return;
+    setActivePage(page);
+  };
 
-    useEffect(() => {
-        console.log("activePage: ", activePage);
-    }, [activePage]);
+  useEffect(() => {
+    console.log("activePage: ", activePage);
+  }, [activePage]);
 
-    return (
-        <>
-            <Header navigateToPage={navigateToPage} />
-            <Component {...pageProps} navigateToPage={navigateToPage} activePage={activePage} />
-        </>
-    );
+  return (
+    <>
+      <Header navigateToPage={navigateToPage} />
+      <Component
+        {...pageProps}
+        navigateToPage={navigateToPage}
+        activePage={activePage}
+      />
+    </>
+  );
 }
