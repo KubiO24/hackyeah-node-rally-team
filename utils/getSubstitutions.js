2958330fba633fb46da5362565b19db7dcfc820a
Igor Białek <igorbialek0@gmail.com>
2023-09-30 17:22:36 +0200

    loading and connection

--- utils/getSubstitutions.js
+++ utils/getSubstitutions.js
@@ -1,17 +1,16 @@
 import OpenAI from "openai";
+import { ENV } from "../constants/env";
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: ENV.OPENAI_API_KEY,
+  dangerouslyAllowBrowser: true,
 });
 
-//Receipe format:
-// ${recipe title} ingredient: list of ingredient
-
-export default async function handler(req, res) {
+export default async function getSubstitutions(recipe) {
   let prompt =
     "You are professional dietician. Please check this recipe and tell me if it is healthy. If any ingredient has better substitute change recipe to use it and all descriptions why it is better to use it will nutritions facts. Next you should output only numerated list of only ingredient substitution that will make big impact on recipe health, nothing more in format '{ingredient: ..., substitution: ..., reason: ...}', You cannot display ingredient in list when no substitution is needed. Overall format should be : '{healthProcent: ..., list: }', it has to be valid json. No prose. Here is recipe:";
 
-  prompt += req.body.recipe;
+  prompt += recipe;
 
   const chatCompletion = await openai.chat.completions.create({
     messages: [{ role: "user", content: prompt }],
@@ -23,9 +22,9 @@ export default async function handler(req, res) {
     let data = JSON.parse(chatCompletion.choices[0].message.content);
     console.log(data);
 
-    res.status(200).json({ data: data });
+    return data;
   } catch (err) {
     console.log(err);
-    res.status(400).json({ data: err.message });
+    return err;
   }
 }
