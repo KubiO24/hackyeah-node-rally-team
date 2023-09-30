d2db2b744079e3fe49474cbec083617a83f1b332
Igor Białek <igorbialek0@gmail.com>
2023-10-01 01:59:08 +0200

    stock view

--- components/Nutrictions/index.js
+++ components/Nutrictions/index.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styles from "./styles.module.css";
 import {
   BarChart,
@@ -15,6 +16,8 @@ export default function Nutrictions({
   ingredientsNutrition,
   substitutionsNutrition,
 }) {
+  const [advancedMode, setAdvancedMode] = useState(false);
+
   let ingredientData = Object.values(ingredientsNutrition.totalNutrients ?? {});
   let substituteData = Object.values(
     substitutionsNutrition.totalNutrients ?? {}
@@ -50,36 +53,78 @@ export default function Nutrictions({
 
   console.log(dividedIngredientData);
 
+  function extractObjectsToArray(inputObject) {
+    const resultArray = [];
+
+    for (const key in inputObject) {
+      if (Array.isArray(inputObject[key])) {
+        resultArray.push(...inputObject[key]);
+      }
+    }
+
+    return resultArray;
+  }
+
+  const ingredientDataArray = extractObjectsToArray(dividedIngredientData);
+
   return (
     <div className={styles.container}>
-      {Object.keys(dividedIngredientData).map((unit) => (
-        <ResponsiveContainer width="100%" height="100%">
-          <BarChart
-            width={500}
-            height={300}
-            data={dividedIngredientData[unit]}
-            margin={{
-              top: 5,
-              right: 30,
-              left: 20,
-              bottom: 5,
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="label" />
-            <YAxis
-              label={{ value: unit, position: "insideLeft", offset: -10 }}
-            />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="quantity" fill="#8884d8" />
-            <Bar dataKey="substitution_quantity" fill="#82ca9d" />
-          </BarChart>
-        </ResponsiveContainer>
-      ))}
-
-      <div>SUBSTITUTION NUTRITION</div>
-      <div>{JSON.stringify(substitutionsNutrition, null, 5)}</div>
+      <label>
+        <input
+          type="checkbox"
+          checked={advancedMode}
+          onChange={() => {
+            setAdvancedMode((prevState) => !prevState);
+          }}
+        />
+        <span></span>
+      </label>
+      {advancedMode ? (
+        Object.keys(dividedIngredientData).map((unit) => (
+          <ResponsiveContainer width="100%" height="100%">
+            <BarChart
+              width={500}
+              height={300}
+              data={dividedIngredientData[unit]}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="label" />
+              <YAxis
+                label={{ value: unit, position: "insideLeft", offset: -10 }}
+              />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="quantity" fill="#8884d8" />
+              <Bar dataKey="substitution_quantity" fill="#82ca9d" />
+            </BarChart>
+          </ResponsiveContainer>
+        ))
+      ) : (
+        <div>
+          {ingredientDataArray.map((data) => (
+            <div>
+              <h2>
+                {data.label}: {data.quantity.toFixed(2)} |{" "}
+                {data.substitution_quantity.toFixed(2)}
+              </h2>
+              <h2>
+                (
+                {(
+                  (data.substitution_quantity / data.quantity - 1) *
+                  100
+                ).toFixed(2)}
+                %)
+              </h2>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
