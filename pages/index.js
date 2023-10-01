152b45153b580dd237eede7158c9ccda717eaf65
Igor Świerczyński <swierczynskigor@gmail.com>
2023-10-01 04:58:04 +0200

    styling

--- pages/index.js
+++ pages/index.js
@@ -6,29 +6,38 @@ import Training from "../components/Training";
 import MOCK_NUTRITIONS from "../MOCK_NUTRITIONS.json";
 
 export default function Home({ navigateToPage, activePage }) {
-    const [ingredientsNutrition, setIngredientsNutrition] = useState(MOCK_NUTRITIONS);
-    const [substitutionsNutrition, setSubstitutionsNutrition] = useState(MOCK_NUTRITIONS);
+  const [ingredientsNutrition, setIngredientsNutrition] =
+    useState(MOCK_NUTRITIONS);
+  const [substitutionsNutrition, setSubstitutionsNutrition] =
+    useState(MOCK_NUTRITIONS);
 
-    return (
-        <>
-            {activePage === "userForm" && <UserForm navigateToPage={navigateToPage} />}
-            {activePage === "substitutions" && (
-                <Substitution
-                    navigateToPage={navigateToPage}
-                    setIngredientsNutrition={setIngredientsNutrition}
-                    ingredientsNutrition={ingredientsNutrition}
-                    substitutionsNutrition={substitutionsNutrition}
-                    setSubstitutionsNutrition={setSubstitutionsNutrition}
-                />
-            )}
-            {activePage === "nutrictions" && (
-                <Nutrictions
-                    navigateToPage={navigateToPage}
-                    ingredientsNutrition={ingredientsNutrition}
-                    substitutionsNutrition={substitutionsNutrition}
-                />
-            )}
-            {activePage === "training" && <Training navigateToPage={navigateToPage} />}
-        </>
-    );
+  return (
+    <>
+      {activePage === "userForm" && (
+        <UserForm navigateToPage={navigateToPage} />
+      )}
+      {activePage === "substitutions" && (
+        <Substitution
+          navigateToPage={navigateToPage}
+          setIngredientsNutrition={setIngredientsNutrition}
+          ingredientsNutrition={ingredientsNutrition}
+          substitutionsNutrition={substitutionsNutrition}
+          setSubstitutionsNutrition={setSubstitutionsNutrition}
+        />
+      )}
+      {activePage === "nutrictions" && (
+        <Nutrictions
+          navigateToPage={navigateToPage}
+          ingredientsNutrition={ingredientsNutrition}
+          substitutionsNutrition={substitutionsNutrition}
+        />
+      )}
+      {activePage === "training" && (
+        <Training
+          navigateToPage={navigateToPage}
+          ingredientsNutrition={ingredientsNutrition}
+        />
+      )}
+    </>
+  );
 }
